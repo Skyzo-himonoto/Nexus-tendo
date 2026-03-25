1d5c9fd57967d6776cb22b2e75ac0765c5916fec
Skyzo-himonoto <skyzoc1a@gmail.com>
2026-03-25 18:46:32 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@
  * ==============================================
  */
 
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const Pino = require('pino');
 const chalk = require('chalk');
 const fs = require('fs-extra');
