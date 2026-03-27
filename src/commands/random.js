10249a381be2d1a351f6e83803fd99dd6747738a
Skyzo-himonoto <skyzoc1a@gmail.com>
2026-03-27 20:04:35 +0700

    Update random.js

--- src/commands/random.js
+++ src/commands/random.js
@@ -1,97 +1,86 @@
-const axios = require('axios');
-const config = require('../../config');
+import axios from 'axios';
+import { randomItem } from '../../lib/utils.js';
 
-async function randomCommand(sock, sender, type) {
-    const randoms = {
-        waifu: 'https://api.waifu.pics/sfw/waifu',
-        neko: 'https://api.waifu.pics/sfw/neko',
-        shinobu: 'https://api.waifu.pics/sfw/shinobu',
-        megumin: 'https://api.waifu.pics/sfw/megumin',
-        bully: 'https://api.waifu.pics/sfw/bully',
-        cuddle: 'https://api.waifu.pics/sfw/cuddle',
-        cry: 'https://api.waifu.pics/sfw/cry',
-        hug: 'https://api.waifu.pics/sfw/hug',
-        awoo: 'https://api.waifu.pics/sfw/awoo',
-        kiss: 'https://api.waifu.pics/sfw/kiss',
-        pat: 'https://api.waifu.pics/sfw/pat',
-        smug: 'https://api.waifu.pics/sfw/smug',
-        bonk: 'https://api.waifu.pics/sfw/bonk',
-        yeet: 'https://api.waifu.pics/sfw/yeet',
-        blush: 'https://api.waifu.pics/sfw/blush',
-        smile: 'https://api.waifu.pics/sfw/smile',
-        wave: 'https://api.waifu.pics/sfw/wave',
-        highfive: 'https://api.waifu.pics/sfw/highfive',
-        handhold: 'https://api.waifu.pics/sfw/handhold',
-        nom: 'https://api.waifu.pics/sfw/nom',
-        meme: 'https://meme-api.com/gimme',
-        memes: 'https://meme-api.com/gimme/50',
-        quote: 'https://api.quotable.io/random',
-        animequote: 'https://animechan.xyz/api/random',
-        cat: 'https://api.thecatapi.com/v1/images/search',
-        dog: 'https://api.thedogapi.com/v1/images/search',
-        fox: 'https://randomfox.ca/floof/',
-        panda: 'https://some-random-api.com/animal/panda',
-        bird: 'https://some-random-api.com/animal/bird',
-        catfact: 'https://catfact.ninja/fact',
-        dogfact: 'https://dog-api.kinduff.com/api/facts',
-        wallpaper: 'https://wall.alphacoders.com/api2.0/get.php',
-        joke: 'https://v2.jokeapi.dev/joke/Any?type=single',
-        dadjoke: 'https://icanhazdadjoke.com/',
-        chucknorris: 'https://api.chucknorris.io/jokes/random',
-        islamquote: 'https://api.islamic-api.com/api/quote',
-        doa: 'https://api.islamic-api.com/api/doa',
-        asmaulhusna: 'https://api.islamic-api.com/api/asmaulhusna'
-    };
+export default async function random(context) {
+  const { sock, sender, args } = context;
+  
+  if (args.length === 0) {
+    return await sock.sendMessage(sender, {
+      text: `📝 *Cara penggunaan:*\n.random <jenis>\n\n📋 *Jenis random:*\n- random kata\n- random quotes\n- random fakta\n- random meme\n- random pantun\n- random cerpen\n- random cat\n- random dog\n\nContoh: .random quotes`
+    });
+  }
+  
+  const type = args[0].toLowerCase();
+  
+  await sock.sendMessage(sender, {
+    text: '🎲 *Mengambil kata random...*'
+  });
+  
+  try {
+    let result;
     
-    const captions = {
-        waifu: '🌸 *WAIFU RANDOM*', neko: '🐱 *NEKO RANDOM*',
-        meme: '😂 *MEME RANDOM*', quote: '💬 *QUOTE RANDOM*',
-        cat: '🐱 *CAT RANDOM*', dog: '🐶 *DOG RANDOM*',
-        joke: '😄 *JOKE RANDOM*', dadjoke: '👨 *DAD JOKE*'
-    };
-    
-    if (!randoms[type]) {
-        const randomButtons = Object.keys(randoms).slice(0, 20).map((key, i) => ({
-            index: i + 1,
-            quickReplyButton: { displayText: key.toUpperCase(), id: `${config.prefix}random ${key}` }
-        }));
+    switch (type) {
+      case 'quotes':
+        const quoteRes = await axios.get('https://api.quotable.io/random');
+        result = `💬 *Quote of the moment*\n\n"${quoteRes.data.content}"\n\n- ${quoteRes.data.author}`;
+        break;
+        
+      case 'fakta':
+        const faktaRes = await axios.get('https://api.ryzendesu.vip/api/random/faktaunik');
+        result = `🔍 *Fakta Unik*\n\n${faktaRes.data.data}`;
+        break;
+        
+      case 'pantun':
+        const pantunRes = await axios.get('https://api.ryzendesu.vip/api/random/pantun');
+        result = `🎭 *Pantun*\n\n${pantunRes.data.data}`;
+        break;
+        
+      case 'meme':
+        const memeRes = await axios.get('https://meme-api.com/gimme');
         await sock.sendMessage(sender, {
-            text: `🎲 *RANDOM GENERATOR*\n\nPilih kategori:\n${Object.keys(randoms).slice(0, 20).join(', ')}\n\nKlik tombol di bawah!`,
-            templateButtons: randomButtons.slice(0, 10)
+          image: { url: memeRes.data.url },
+          caption: `😂 *Meme*\n\n📝 *Title:* ${memeRes.data.title}\n👍 *Upvotes:* ${memeRes.data.ups}`
         });
         return;
-    }
-    
-    await sock.sendMessage(sender, { text: `🔄 Mengambil random ${type}...` });
-    
-    try {
-        const response = await axios.get(randoms[type]);
-        let result;
         
-        switch(type) {
-            case 'meme':
-                result = response.data;
-                await sock.sendMessage(sender, { image: { url: result.url }, caption: `😂 *${result.title}*\n👍 ${result.ups} | 💬 ${result.comments}` });
-                break;
-            case 'quote':
-                result = response.data;
-                await sock.sendMessage(sender, { text: `💬 *QUOTE*\n\n"${result.content}"\n- ${result.author}` });
-                break;
-            case 'cat':
-            case 'dog':
-                result = response.data[0];
-                await sock.sendMessage(sender, { image: { url: result.url }, caption: `${captions[type] || '✨ RANDOM'}` });
-                break;
-            case 'joke':
-                result = response.data;
-                await sock.sendMessage(sender, { text: `😄 *JOKE*\n\n${result.joke || result.setup + ' ' + result.delivery}` });
-                break;
-            default:
-                await sock.sendMessage(sender, { image: { url: response.data.url || response.data.image || response.data.file }, caption: captions[type] || `✨ *${type.toUpperCase()} RANDOM*` });
-        }
-    } catch (error) {
-        await sock.sendMessage(sender, { text: '❌ Gagal mengambil random!' });
+      case 'cat':
+        const catRes = await axios.get('https://api.thecatapi.com/v1/images/search');
+        await sock.sendMessage(sender, {
+          image: { url: catRes.data[0].url },
+          caption: '🐱 *Random Cat*'
+        });
+        return;
+        
+      case 'dog':
+        const dogRes = await axios.get('https://dog.ceo/api/breeds/image/random');
+        await sock.sendMessage(sender, {
+          image: { url: dogRes.data.message },
+          caption: '🐕 *Random Dog*'
+        });
+        return;
+        
+      case 'kata':
+        const kataList = [
+          'Jangan menyerah, karena kamu hebat!',
+          'Setiap hari adalah kesempatan baru',
+          'Kesuksesan dimulai dari mimpi',
+          'Percayalah pada proses',
+          'Jadilah versi terbaik dari dirimu'
+        ];
+        result = `💪 *Kata Motivasi*\n\n"${randomItem(kataList)}"`;
+        break;
+        
+      default:
+        return await sock.sendMessage(sender, {
+          text: `❌ Jenis random *${type}* tidak dikenal!\n\nGunakan: quotes, fakta, pantun, meme, cat, dog, kata`
+        });
     }
+    
+    await sock.sendMessage(sender, { text: result });
+    
+  } catch (err) {
+    await sock.sendMessage(sender, {
+      text: `❌ *Error:* ${err.message}`
+    });
+  }
 }
-
-module.exports = { randomCommand };
