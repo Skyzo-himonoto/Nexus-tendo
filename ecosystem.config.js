837288a9db75a5514f7d6c3fe4e1f830bd6699b4
Skyzo-himonoto <skyzoc1a@gmail.com>
2026-03-27 21:07:42 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
-module.exports = {
+export default {
   apps: [{
-    name: 'nexus-tendo-md',
-    script: 'src/main.js',
+    name: 'nexus-md',
+    script: 'index.js',
+    cwd: './',
     watch: false,
     autorestart: true,
-    max_memory_restart: '300M',
+    max_memory_restart: '500M',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      NODE_OPTIONS: '--max-old-space-size=512'
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
